d7520145776e01450f377e4b6cea6039103d3a2e
mohammadrezaetaati <mohammadreza.etaati94@yahoo.com>
2023-01-07 10:04:36 +0330

    add pernissions

---
+++ utils/permissions.py
@@ -0,0 +1,36 @@
+from rest_framework.permissions import IsAuthenticated
+
+
+                
+
+class IsTechnicianPhoneline(IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and \
+            request.user.role == 'technician_phoneline' or\
+            request.user.role == 'admin'
+
+
+
+class IsTechnicianDevice(IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and \
+                request.user.role == 'technician_device' or\
+                request.user.role == 'admin'
+
+
+class IsStorekepeer(IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and \
+                    request.user.role == 'storekepeer' or\
+                    request.user.role == 'admin'
+
+
+class IsInspector(IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and \
+                        request.user.role == 'inspector' or\
+                        request.user.role == 'admin'
